d5eeb05045aa1ede7ce9282c1d378df712b1b493
akhileshkumar011 <akhileshkumar31806@gmail.com>
2026-07-25 17:16:57 +0530

    variables and Datatypes

---
+++ lecture40/index.js
@@ -0,0 +1,38 @@
+// var age = 25;
+
+// if(true){
+//     console.log(age)
+// }
+
+// function solve(){
+//     var age = 25;
+//     console.log(age);
+// }
+// console.log(age)
+// solve();
+
+// let x = 4;
+// console.log(x);
+
+// let a = 5 ;
+// a = "akhilesh";
+// console.log(a);
+
+// let a = 10;
+// a = babbar;
+// a = true;
+// a = null;
+
+// const ram = 28;
+// console.log(a);
+
+// let marks = 23.333;
+// marks = "babbar";
+// marks =true;
+
+// let marks = null;
+// console.log(marks);
+
+
+let marks = 298459820520103409582095820902903485;
+console.log(marks);
