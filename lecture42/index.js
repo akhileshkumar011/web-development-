26b94c6a269ae8a4b790104452f5b8687e0988db
akhileshkumar011 <akhileshkumar31806@gmail.com>
2026-07-27 23:23:38 +0530

    loops and strings

---
+++ lecture42/index.js
@@ -0,0 +1,57 @@
+//Print hello 10 times
+
+for(let i=1;i<=10;i++){
+    console.log("hello");
+}
+
+let i =1;
+while(i<=10){
+    console.log('hello world')
+    i++;
+}
+
+//Print 1 to 5
+
+for(let i=1;i<=5;i++){
+    console.log(i);
+}
+
+let p=1;
+while(p<=5){
+    console.log(i);
+    p++;
+}
+
+//print 5 to 1
+
+for(let i=5;i>0;i--){
+    console.log(i);
+}
+
+let m=5;
+while(m>0){
+    console.log(m);
+    m--;
+}
+
+//break
+
+for(let i=0;i<=5;i++){
+    if(i==5){
+        break;
+    }
+    else{
+        console.log(i);
+    }
+}
+
+//continue
+
+for(let i =0;i<=4;i++){
+    if(i==3){
+        continue;
+    }
+    else{
+        console.log(i);
+    }
+}
